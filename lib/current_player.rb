9fb6f1d5c52b9e138afdf0c77019e7d47f5b01b5
Chris Baum <christopher.baum@valpo.edu>
2020-12-30 01:23:52 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,3 +1,5 @@
+
+board1 = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
 def turn_count(board)
   board.each do |position|
     turns = 0
@@ -7,6 +9,7 @@ def turn_count(board)
     end
   end
 end
+turn_count(board1)
 
 def current_player
 
