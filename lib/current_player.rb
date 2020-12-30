e809240de8c51293f005a7e2f8f78ee279b09d53
Chris Baum <christopher.baum@valpo.edu>
2020-12-30 01:21:16 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,11 @@
 def turn_count(board)
-
+  board.each do |position|
+    turns = 0
+    if position == "X" || position == "O"
+      turns += 1
+    else
+    end
+  end  
 end
 
 def current_player
