977b8ced83b5d87837ec1fb20aa3787f19f30c75
Chris Baum <christopher.baum@valpo.edu>
2020-12-30 01:40:11 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,4 @@
 
-#board1 = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
 def turn_count(board)
   turns = 0
   board.each do |position|
@@ -10,13 +9,9 @@ def turn_count(board)
   end
   turns
 end
-#turn_count(board1)
 
 def current_player(board)
   odd_or_even = turn_count(board) % 2
-  #puts "#{odd_or_even}"
   odd_or_even == 0 ? player = "X" : player = "O"
-  #puts "#{player}"
   player
 end
-#current_player(board1)
