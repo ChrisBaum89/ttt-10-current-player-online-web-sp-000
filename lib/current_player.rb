55d0e51fd61d0e7f3be5115ef93e709c446dfa7e
Chris Baum <christopher.baum@valpo.edu>
2020-12-30 01:44:38 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,6 +3,7 @@ def turn_count(board)
   turns = 0 #sets initial turn value
   board.each do |position|
     position == "O" || position == "X" ? turns += 1 : nil#counts number of turns that have been taken
+  end
   turns #returns the value of turns
 end
 
