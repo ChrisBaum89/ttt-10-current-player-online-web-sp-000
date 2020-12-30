0764430cf5d730631a246646811fe42441902686
Chris Baum <christopher.baum@valpo.edu>
2020-12-30 01:24:28 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,7 +3,7 @@ board1 = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
 def turn_count(board)
   board.each do |position|
     turns = 0
-    if true
+    if position == "O"
       puts "position"
     else
     end
