bbd8013d8252a87a807fda7e5a5f93962ee7d009
Chris Baum <christopher.baum@valpo.edu>
2020-12-30 01:41:22 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,17 +1,17 @@
 
 def turn_count(board)
-  turns = 0
+  turns = 0 #sets initial turn value
   board.each do |position|
-    if position == "O" || position == "X"
+    if position == "O" || position == "X" #counts number of turns that have been taken
       turns += 1
     else
     end
   end
-  turns
+  turns #returns the value of turns
 end
 
 def current_player(board)
   odd_or_even = turn_count(board) % 2
   odd_or_even == 0 ? player = "X" : player = "O"
-  player
+  player #returns current player "X" or "O"
 end
